251a482ae5eee5ad091d906f5b380be2e41837ea
Franklin <Franklin@DESKTOP-OQO8VFM>
2019-04-14 17:33:47 -0500

    4.- creacion de la interfaz con sus metodos abstractos

--- src/ec/edu/ups/interfaz/Interfaz.java
+++ src/ec/edu/ups/interfaz/Interfaz.java
@@ -1,14 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ec.edu.ups.interfaz;
 
 /**
- *
- * @author Franklin
+ * 
+ * Esta clase genera metodos abstractos para implementar en las clases hijas
+ * 
+ * @author FranklinOrellana
+ * 
  */
+
 public interface Interfaz {
     
+    /**
+     * Declarar metodos abstractos
+     */
+
+    public abstract void partidaInicio();
+    public abstract void partidaTerminada();
+
 }
+
